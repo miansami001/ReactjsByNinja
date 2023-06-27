a1d8965155468a804b9b19fe8891203a9e9a3fd4
Sami Ullah <sami.ullah@2bvision.com>
2023-06-27 08:00:07 -0700

    SecondCode

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App appname">
       <Navbar />
       <div className="content">
         <Home />
