539019136dbc9a181f3518cee87f20d4a6d32119
miansami001 <miansami6426@gmail.com>
2023-07-01 03:04:04 -0700

    useEffect Hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -10,10 +10,14 @@ const Home = () => {
     ]);
 
     const handleDelete = (id) => {
-        debugger;
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('use effect run...');
+        console.log(blogs);
+    });
 
 
     return (
