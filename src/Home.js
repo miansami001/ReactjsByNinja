a631f15dcde7963ffbeada3c7f73647eb6a2995a
miansami001 <miansami6426@gmail.com>
2023-07-13 15:36:01 -0700

    Lec # 17: Fetching data with UseEffect

--- src/Home.js
+++ src/Home.js
@@ -3,31 +3,22 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const [name, setName] = useState('Mian');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('use effect run...');
-        console.log(name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+        })
+    }, []);
 
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
-            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blog!" handleDelete={handleDelete} /> */}
-            <button onClick={() => setName('MianSami')}>Change Name</button>
-            <p>{name}</p>
+            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
     );
 }
