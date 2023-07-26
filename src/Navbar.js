da0b9c28266b25a5434b3ba7534b4b6176945676
miansami001 <miansami6426@gmail.com>
2023-07-26 12:06:31 +0500

    React router

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Mian's System</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                {/* <a href="/">Home</a> */}
+                <Link to="/">Home</Link>
+                {/* <a href="/create">New Blog</a> */}
+                <Link to="/create">New Blogs</Link>
             </div>
         </nav>
     );
